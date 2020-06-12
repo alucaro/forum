260c87268fb5f1d4303c47c51b018c4a363c5a2e
Adriana Campo <alucaro29a@hotmail.com>
2020-06-11 22:53:59 -0500

    How to test validation errors

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -33,16 +33,66 @@ class CreateThreadsTest extends TestCase
         $this->signIn();
 
         //When we hit the endpoint to create a new thread
-        $thread = create('App\Thread');
+        //$thread = create('App\Thread');
         //dd($thread->title);
-        $this->post('/threads', $thread->toArray());
+        //$this->post('/threads', $thread->toArray());
+
+        //I need create a user to get the id,or I can use $response->headres to simulate one
+        $thread = make('App\Thread');
+        $response = $this->post('/threads', $thread->toArray());
 
         //Then, when we visit the thread,
         //We should see the new thread
-        $this->get($thread->path())
+        //$this->get($thread->path())
+        $this->get($response->headers->get('location'))
              ->assertSee($thread->title)
              ->assertSee($thread->body);
 
+    }
+
+    /** @test */
+
+    function a_thread_requires_a_title()
+    {
+        $this->publishThread(['title' => null])
+            ->assertSessionHasErrors('title');
+        /*
+        $this->withExceptionHandling();
+        $this->signIn();
+
+        $thread = make('App\Thread', ['title' => null]);
+
+        $this->post('/threads', $thread->toArray())
+            ->assertSessionHasErrors('title');
+        */
+    }
+
+    /** @test */
+    function a_thread_requires_a_body()
+    {
+        $this->publishThread(['body' => null])
+            ->assertSessionHasErrors('body');
+    }
+
+    /** @test */
+    function a_thread_requires_a_valid_channel()
+    {
+        factory('App\Channel', 2);
+
+        $this->publishThread(['channel_id' => null])
+            ->assertSessionHasErrors('channel_id');
+        
+        $this->publishThread(['channel_id' => 999])
+            ->assertSessionHasErrors('channel_id');
+    }
+
+    public function publishThread($overrides =[])
+    {
+        $this->withExceptionHandling()->signIn();
+
+        $thread = make('App\Thread', $overrides);
+
+        return $this->post('/threads', $thread->toArray());
     }
 
 }
