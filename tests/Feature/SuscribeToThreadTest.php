6ceb5e2353c1dbaac2849187ac7c977c9477e993
Adriana Campo <alucaro29a@hotmail.com>
2020-09-03 15:08:45 -0500

    Thread suscriptions part 2

---
+++ tests/Feature/SuscribeToThreadTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+
+class SuscribeToThreadTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function a_user_can_suscribe_to_thread()
+    {
+        // $this->withoutExceptionHandling();
+
+        $this->signIn();
+
+        //Given we have a Thread
+        $thread = create('App\Thread');
+
+        //and the user suscribe to the thread
+        $this->post($thread->path() . '/suscriptions');
+
+        //Then, each time a new reply is left
+        $thread->addReply([
+            'user_id' => auth()->id(),
+            'body' => 'Some reply here'
+        ]);
+
+        //A notification should be prepared for the user
+        // $this->assertCount(1, auth()->user()->notifications);
+    }
+}
